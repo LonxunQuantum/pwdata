ab00d7967b9e7a5d7e3d413ee08b3fd18095d131
yeahoool <a471101014@gmail.com>
2024-02-28 13:04:27 +0800

    update gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pwdata", 
-    version="2024.2.29",
+    version="2024.2.29+",
     author="LonxunQuantum",
     author_email="lonxun@pwmat.com",
     description="pwdata is a data pre-processing tool for PWMLFF, which can be used to extract features and labels. It also provides convenient interfaces for data conversion between different software.",
